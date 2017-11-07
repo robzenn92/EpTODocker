0ee4fe8777c413cd55182f76a3836a9d7f75f41d
Roberto Zen <rob.zenn92@gmail.com>
2017-11-07 21:28:34 +0100

    StabilityOracle added

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ sh run_tests.sh
 if [ $? -eq 0 ]; then
     eval $(minikube docker-env)
     kubectl delete deployment epto-deployment
-    sh ./cyclon/build.sh
-    #sh ./epto/build.sh
+#    sh ./cyclon/build.sh
+    sh ./epto/build.sh
     kubectl create -f deployment.yml
     kubectl get pod -o wide
 fi
