9377983c9e57f831bb3e95db23054d631fdc8dfc
Roberto Zen <rob.zenn92@gmail.com>
2017-12-13 09:59:33 +0100

    build.sh updated with minikube status check, closes #10

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+echo "Checking Minikube Status"
+minikube status > /dev/null
+if [ $? -eq 1 ]; then
+    echo "Minikube status was Stopped. It' will start in a while."
+    minikube start
+fi
 #echo "Running tests."
 #sh run_tests.sh
-if [ $? -eq 0 ]; then
+#if [ $? -eq 0 ]; then
     echo "Building Cyclon.."
     sh cyclon_project/build.sh
 #    echo "Building EpTO.."
 #    sh epto/build.sh
-fi
+#fi
