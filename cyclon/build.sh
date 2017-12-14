15109cf3b3041d8bf5f57917e0ff1c5295090999
Roberto Zen <rob.zenn92@gmail.com>
2017-12-14 15:28:37 +0100

    Cyclon Flask and EpTO Flask folders deleted

--- cyclon/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-eval $(minikube docker-env)
-docker build -f cyclon/Dockerfile -t cyclon:latest .
--- epto/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-eval $(minikube docker-env)
-docker build -f epto/Dockerfile -t epto:latest .
